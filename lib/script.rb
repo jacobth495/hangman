b173e0a6078203263f74860f3c6d8d8588402ee8
Jacob Henderson <jacobth495@gmail.com>
2022-10-06 11:23:22 -0500

    Gave initialize method a way to select a random word from the text file

--- lib/script.rb
+++ lib/script.rb
@@ -1,5 +1,23 @@
+# frozen_string_literal: true
+
 class Board
+  attr_accessor :answer
+
   def initialize
-    
+    words = []
+    File.foreach('google_10000_words.txt') do |word|
+      words << word.strip
+    end
+    @answer = words[rand(1..9894)].split('')
   end
+
+  def show_answer
+    p answer
+  end
+end
+
+myboard = Board.new
+
+3.times do
+  myboard.show_answer
 end
